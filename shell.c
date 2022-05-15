ef614dc768e4e9d3ea57043e9a97fa1799ea05cc
belovetech <belovetech@gmail.com>
2022-05-15 15:09:56 +0100

    [Update] main program

--- shell.c
+++ shell.c
@@ -1,95 +1,27 @@
 #include "shell.h"
-
 /**
- * main - Simple Shell (Hsh)
- * @argc: Argument Count
- * @argv:Argument Value
- * Return: Exit Value By Status
+ * main - shell skeleton
+ * @argc: number of arguments in the input
+ * @argv: arguments in the input
+ * @envp: environment variables
+ * Return: 0 on Success
  */
-
-int main(__attribute__((unused)) int argc, char **argv)
+int main(int argc, char *argv[], char *envp[])
 {
-	char *input, **cmd;
-	int counter = 0, statue = 1, st = 0;
 
-	if (argv[1] != NULL)
-		read_file(argv[1], argv);
-	signal(SIGINT, signal_to_handel);
-	while (statue)
+	if (argc == 1)
 	{
-		counter++;
-		if (isatty(STDIN_FILENO))
-			prompt();
-		input = _getline();
-		if (input[0] == '\0')
-		{
-			continue;
-		}
-		history(input);
-		cmd = parse_cmd(input);
-		if (_strcmp(cmd[0], "exit") == 0)
-		{
-			exit_bul(cmd, input, argv, counter);
-		}
-		else if (check_builtin(cmd) == 0)
-		{
-			st = handle_builtin(cmd, st);
-			free_all(cmd, input);
-			continue;
-		}
-		else
-		{
-			st = check_cmd(cmd, input, counter, argv);
-
-		}
-		free_all(cmd, input);
+		_noargv(argv, envp);
 	}
-	return (statue);
-}
-
-/**
- * check_builtin - check builtin
- *
- * @cmd:command to check
- * Return: 0 Succes -1 Fail
- */
-
-int check_builtin(char **cmd)
-{
-	bul_t fun[] = {
-		{"cd", NULL},
-		{"help", NULL},
-		{"echo", NULL},
-		{"history", NULL},
-		{NULL, NULL}
-	};
-	int i = 0;
-		if (*cmd == NULL)
+	else if (argc == 2)
 	{
-		return (-1);
+		_yesargv(argv, envp);
 	}
-
-	while ((fun + i)->command)
+	else
 	{
-		if (_strcmp(cmd[0], (fun + i)->command) == 0)
-			return (0);
-		i++;
+		write(STDIN_FILENO, "NO ADMITTED AMOUNT OF ARGUMENTS", 31);
+		write(STDIN_FILENO, "\n", 1);
 	}
-	return (-1);
-}
-
-/**
- * creat_envi - Creat Array of Enviroment Variable
- * @envi: Array of Enviroment Variable
- * Return: Void
- */
-
-void creat_envi(char **envi)
-{
-	int i;
-
-	for (i = 0; environ[i]; i++)
-		envi[i] = _strdup(environ[i]);
-	envi[i] = NULL;
+	return (0);
 }
 
