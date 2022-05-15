dbbe272cee3818cc13dd985962aa5c37e13a95f7
belovetech <belovetech@gmail.com>
2022-05-15 14:07:15 +0100

    [update] the main program

--- shell.c
+++ shell.c
@@ -1,73 +1,95 @@
-#include "shell.c"
+#include "shell.h"
 
 /**
- * free_data - frees data structure
- * @datash: data structure
- * Return: no return
+ * main - Simple Shell (Hsh)
+ * @argc: Argument Count
+ * @argv:Argument Value
+ * Return: Exit Value By Status
  */
-void free_data(data_shell *datash)
+
+int main(__attribute__((unused)) int argc, char **argv)
 {
-	unsigned int i;
+	char *input, **cmd;
+	int counter = 0, statue = 1, st = 0;
 
-	for (i = 0; datash->_environ[i]; i++)
+	if (argv[1] != NULL)
+		read_file(argv[1], argv);
+	signal(SIGINT, signal_to_handel);
+	while (statue)
 	{
-		free(datash->_environ[i]);
-	}
+		counter++;
+		if (isatty(STDIN_FILENO))
+			prompt();
+		input = _getline();
+		if (input[0] == '\0')
+		{
+			continue;
+		}
+		history(input);
+		cmd = parse_cmd(input);
+		if (_strcmp(cmd[0], "exit") == 0)
+		{
+			exit_bul(cmd, input, argv, counter);
+		}
+		else if (check_builtin(cmd) == 0)
+		{
+			st = handle_builtin(cmd, st);
+			free_all(cmd, input);
+			continue;
+		}
+		else
+		{
+			st = check_cmd(cmd, input, counter, argv);
 
-	free(datash->_environ);
-	free(datash->pid);
+		}
+		free_all(cmd, input);
+	}
+	return (statue);
 }
 
 /**
- * set_data - Initialize data structure
- * @datash: data structure
- * @av: argument vector
- * Return: no return
+ * check_builtin - check builtin
+ *
+ * @cmd:command to check
+ * Return: 0 Succes -1 Fail
  */
 
-void set_data(data_shell *datash, char **av)
+int check_builtin(char **cmd)
 {
-	unsigned int i;
-
-	datash->av = av;
-	datash->input = NULL;
-	datash->args = NULL;
-	datash->status = 0;
-	datash->counter = 1;
-
-	for (i = 0; environ[i]; i++)
-		;
-
-	datash->_environ = malloc(sizeof(char *) * (i + 1));
-
-	for (i = 0; environ[i]; i++)
+	bul_t fun[] = {
+		{"cd", NULL},
+		{"help", NULL},
+		{"echo", NULL},
+		{"history", NULL},
+		{NULL, NULL}
+	};
+	int i = 0;
+		if (*cmd == NULL)
 	{
-		datash->_environ[i] = _strdup(environ[i]);
+		return (-1);
 	}
 
-	datash->_environ[i] = NULL;
-	datash->pid = aux_itoa(getpid());
+	while ((fun + i)->command)
+	{
+		if (_strcmp(cmd[0], (fun + i)->command) == 0)
+			return (0);
+		i++;
+	}
+	return (-1);
 }
 
 /**
- * main - Entry point
- *
- * @ac: argument count
- * @av: argument vector
- *
- * Return: 0 on success.
+ * creat_envi - Creat Array of Enviroment Variable
+ * @envi: Array of Enviroment Variable
+ * Return: Void
  */
-int main(int ac, char **av)
-{
-	data_shell datash;
-	(void) ac;
 
-	signal(SIGINT, get_sigint);
-	set_data(&datash, av);
-	shell_loop(&datash);
-	free_data(&datash);
-	if (datash.status < 0)
-		return (255);
-	return (datash.status);
+void creat_envi(char **envi)
+{
+	int i;
+
+	for (i = 0; environ[i]; i++)
+		envi[i] = _strdup(environ[i]);
+	envi[i] = NULL;
 }
 
