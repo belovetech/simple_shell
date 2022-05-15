2a3d125017032768e608c24a546da179057f1873
Nimah <busayonimota1603@gmail.com>
2022-05-15 12:05:33 +0000

    [Create] header file

---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+/*
+ * File: shell.h
+ * Auth: Alex Yu
+ * Brennan D Baraban
+ */
+
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+#endif /* _SHELL_H_ */
